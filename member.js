d70bdf3fc3254122f354cfb6ea40d01045444d5d
JhonyRetro <94227499+JhonyRetro@users.noreply.github.com>
2024-09-17 10:02:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 26,
+        skills: {
+            programming: ['JavaScript', 'Python', 'Java'],
+            design: ['Photoshop', 'Illustrator', 'Corel Draw']
+        }
+    }
+
+    return member.skills;
+}
